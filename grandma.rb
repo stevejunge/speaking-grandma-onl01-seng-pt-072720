694b810f59bdfbe3f5dbc7168d5fd6c3e7313eb3
Stephen Junge <steve.junge@gmail.com>
2020-07-29 18:23:36 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,11 +1,11 @@
 # Write a speak_to_grandma method.
 def speak_to_grandma(x)
  if x.upcase
-   puts "NO, NOT SINCE 1938!"
+   return "NO, NOT SINCE 1938!"
  elsif "I LOVE YOU GRANDMA!"
-  puts  "I LOVE YOU TOO PUMPKIN!"
+  return  "I LOVE YOU TOO PUMPKIN!"
  else
-   puts "HUH?! SPEAK UP, SONNY!"
+   return "HUH?! SPEAK UP, SONNY!"
  end
 end
  
